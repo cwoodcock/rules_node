563ce9042baa01ff6b38692ce04d691243057799
Tomas Miliauskas <tomas@wix.com>
2018-02-22 18:04:31 +0200

    Add enzyme test using jsdom

---
+++ examples/webpack_build/src/components/App/index.spec.ts
@@ -0,0 +1,17 @@
+import 'jsdom-global/register';
+import * as React from 'react';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-15';
+import {expect} from 'chai';
+import App from './index.ts';
+
+const {mount} = Enzyme;
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe('App', () => {
+  it('should pass', () => {
+    const wrapper = mount(React.createElement(App));
+    expect(wrapper.text()).to.equal('hello there');
+  });
+});
